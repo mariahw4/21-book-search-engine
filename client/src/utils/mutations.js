33663867f6b3e4c0c27a7b5ab756260233bbf820
Mariah Wear <wearmariahk@gmail.com>
2023-05-10 11:00:22 -0600

    defining queries and mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,25 +1,80 @@
 import { gql } from '@apollo/client';
 
 
-
-export const CREATE_MATCHUP = gql`
-  mutation createMatchup($tech1: String!, $tech2: String!) {
-    createMatchup(tech1: $tech1, tech2: $tech2) {
+export const ADD_USER = gql`
+mutation addUser($username: String!, $password: String!, $email: String!) {
+  addUser(username: $username, password: $password, email: $email) {
+    
+    user {
       _id
-      tech1
-      tech2
+      username
+      email
+      bookCount
+      savedBooks {
+        authors
+        bookId
+        image
+        link
+        title
+        description
+      }
     }
+    token
   }
+}
 `;
 
-export const CREATE_BOOK = gql`
-  mutation createBook($_id: String!, $techNum: Int!) {
-    createVote(_id: $_id, techNum: $techNum) {
+export const SAVE_BOOK = gql`
+
+    mutation saveBook($input: savedBook!) {
+    saveBook (input: $input)
+        {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                # _id
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+    }
+`;
+
+
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId:$bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                # _id
+                bookId
+                authors
+                image
+                link
+                title
+                description
+            }
+        }
+}
+`;
+
+export const LOGIN_USER = gql`
+mutation loginUser($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
       _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
     }
   }
+}
 `;
